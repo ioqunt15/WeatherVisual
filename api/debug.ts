d91bd41871f1ab7c2911b58c81b9cce560403331
Kim Ji Hun <ioqunt15@gmail.com>
2026-06-19 13:36:52 +0900

    debug: test scenarios import and env

--- api/debug.ts
+++ api/debug.ts
@@ -1,13 +1,19 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import Redis from 'ioredis'
+import { scenarios } from '../src/data/scenarios'
 
 export default async function handler(req: IncomingMessage, res: ServerResponse & { status: (c: number) => any; json: (b: any) => void; end: () => void }) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
 
+  const redisUrl = process.env.REDIS_URL
+  const maskedRedisUrl = redisUrl
+    ? `${redisUrl.slice(0, 15)}... (len: ${redisUrl.length})`
+    : 'undefined'
+
   res.status(200).json({
     success: true,
-    redisType: typeof Redis,
-    redisExport: Redis ? Object.keys(Redis) : null
+    redisUrlStatus: maskedRedisUrl,
+    scenariosCount: scenarios.length,
+    firstScenarioId: scenarios[0]?.id
   })
 }
